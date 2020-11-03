b57dfbf7b5bda366d987e05b9fbd9240530d452a
Dominik Rosiek <drosiek@sumologic.com>
2020-11-03 15:18:51 +0100

    Add method to convert AttributeValue to string

--- exporter/sumologicexporter/filtering.go
+++ exporter/sumologicexporter/filtering.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
@@ -33,13 +34,35 @@ func newFiltering(fields []string) (*filtering, error) {
 	}, nil
 }
 
+// convertAttributeToString returns string which represents given pdata.AttributeValue
+func (f *filtering) convertAttributeToString(value pdata.AttributeValue) string {
+	switch value.Type() {
+	case pdata.AttributeValueSTRING:
+		return value.StringVal()
+	case pdata.AttributeValueBOOL:
+		return strconv.FormatBool(value.BoolVal())
+	case pdata.AttributeValueINT:
+		return strconv.FormatInt(value.IntVal(), 10)
+	case pdata.AttributeValueDOUBLE:
+		return fmt.Sprintf("%g", value.DoubleVal())
+	case pdata.AttributeValueMAP:
+		return ""
+	case pdata.AttributeValueARRAY:
+		return ""
+	case pdata.AttributeValueNULL:
+		return ""
+	}
+	return ""
+}
+
+// filter returns map of strings which matches at least one of the filtering regexes
 func (f *filtering) filter(attributes pdata.AttributeMap) map[string]string {
 	returnValue := make(map[string]string)
 
 	attributes.ForEach(func(k string, v pdata.AttributeValue) {
 		for _, regex := range f.regexes {
 			if regex.MatchString(k) {
-				returnValue[k] = v.StringVal()
+				returnValue[k] = f.convertAttributeToString(v)
 				return
 			}
 		}
@@ -47,6 +70,7 @@ func (f *filtering) filter(attributes pdata.AttributeMap) map[string]string {
 	return returnValue
 }
 
+// filterOut returns map of strings which doesn't match any of the filtering regexes
 func (f *filtering) filterOut(attributes pdata.AttributeMap) map[string]string {
 	returnValue := make(map[string]string)
 
@@ -56,7 +80,7 @@ func (f *filtering) filterOut(attributes pdata.AttributeMap) map[string]string {
 				return
 			}
 		}
-		returnValue[k] = v.StringVal()
+		returnValue[k] = f.convertAttributeToString(v)
 	})
 	return returnValue
 }
--- exporter/sumologicexporter/filtering_test.go
+++ exporter/sumologicexporter/filtering_test.go
@@ -43,3 +43,55 @@ func TestFilterOutMetadata(t *testing.T) {
 	}
 	assert.Equal(t, data, expected)
 }
+
+func TestConvertStringAttributeToString(t *testing.T) {
+	attributes := pdata.NewAttributeMap()
+	attributes.InsertString("key", "test_value")
+
+	regexes := []string{}
+	f, err := newFiltering(regexes)
+	assert.NoError(t, err)
+
+	value, _ := attributes.Get("key")
+	data := f.convertAttributeToString(value)
+	assert.Equal(t, data, "test_value")
+}
+
+func TestConvertIntAttributeToString(t *testing.T) {
+	attributes := pdata.NewAttributeMap()
+	attributes.InsertInt("key", 15)
+
+	regexes := []string{}
+	f, err := newFiltering(regexes)
+	assert.NoError(t, err)
+
+	value, _ := attributes.Get("key")
+	data := f.convertAttributeToString(value)
+	assert.Equal(t, data, "15")
+}
+
+func TestConvertDoubleAttributeToString(t *testing.T) {
+	attributes := pdata.NewAttributeMap()
+	attributes.InsertDouble("key", 4.16)
+
+	regexes := []string{}
+	f, err := newFiltering(regexes)
+	assert.NoError(t, err)
+
+	value, _ := attributes.Get("key")
+	data := f.convertAttributeToString(value)
+	assert.Equal(t, data, "4.16")
+}
+
+func TestConvertBoolAttributeToString(t *testing.T) {
+	attributes := pdata.NewAttributeMap()
+	attributes.InsertBool("key", false)
+
+	regexes := []string{}
+	f, err := newFiltering(regexes)
+	assert.NoError(t, err)
+
+	value, _ := attributes.Get("key")
+	data := f.convertAttributeToString(value)
+	assert.Equal(t, data, "false")
+}
