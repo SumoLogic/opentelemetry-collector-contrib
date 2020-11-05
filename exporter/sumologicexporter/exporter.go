a826d963ba79c79e762accd15fc4ad04b0a67832
Dominik Rosiek <drosiek@sumologic.com>
2020-11-05 15:20:47 +0100

    Use sender in exporter

--- exporter/sumologicexporter/exporter.go
+++ exporter/sumologicexporter/exporter.go
@@ -17,14 +17,18 @@ package sumologicexporter
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenterror"
 	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
 type sumologicexporter struct {
 	config *Config
+	client *http.Client
+	filter filter
 }
 
 func initExporter(cfg *Config) (*sumologicexporter, error) {
@@ -50,8 +54,17 @@ func initExporter(cfg *Config) (*sumologicexporter, error) {
 		return nil, fmt.Errorf("unexpected compression encoding: %s", cfg.CompressEncoding)
 	}
 
+	f, err := newFilter(cfg.MetadataAttributes)
+	if err != nil {
+		return nil, err
+	}
+
 	se := &sumologicexporter{
 		config: cfg,
+		client: &http.Client{
+			Timeout: cfg.TimeoutSettings.Timeout,
+		},
+		filter: f,
 	}
 
 	return se, nil
@@ -79,6 +92,71 @@ func newLogsExporter(
 }
 
 // pushLogsData groups data with common metadata and send them together to Sumo Logic
-func (se *sumologicexporter) pushLogsData(context.Context, pdata.Logs) (droppedTimeSeries int, err error) {
+func (se *sumologicexporter) pushLogsData(_ context.Context, ld pdata.Logs) (int, error) {
+	var (
+		currentMetadata  Fields
+		previousMetadata Fields
+		errors           []error
+		sdr              *sender = newSender(se.config, se.client, se.filter)
+		droppedRecords   []pdata.LogRecord
+	)
+
+	// Iterate over ResourceLogs
+	for i := 0; i < ld.ResourceLogs().Len(); i++ {
+		resource := ld.ResourceLogs().At(i)
+
+		// iterate over InstrumentationLibraryLogs
+		for j := 0; j < resource.InstrumentationLibraryLogs().Len(); j++ {
+			library := resource.InstrumentationLibraryLogs().At(j)
+
+			// iterate over Logs
+			for k := 0; k < library.Logs().Len(); k++ {
+				log := library.Logs().At(k)
+				currentMetadata = sdr.filter.GetMetadata(log.Attributes())
+
+				// If metadata differs from currently buffered, flush the buffer
+				if currentMetadata != previousMetadata && previousMetadata != "" {
+					dropped, err := sdr.sendLogs(previousMetadata)
+					if err != nil {
+						droppedRecords = append(droppedRecords, dropped...)
+						errors = append(errors, err)
+					}
+					sdr.cleanBuffer()
+				}
+
+				// assign metadata
+				previousMetadata = currentMetadata
+
+				// add log to the buffer
+				dropped, err := sdr.batch(log, previousMetadata)
+				if err != nil {
+					droppedRecords = append(droppedRecords, dropped...)
+					errors = append(errors, err)
+				}
+			}
+		}
+	}
+
+	// Flush pending logs
+	dropped, err := sdr.sendLogs(previousMetadata)
+	if err != nil {
+		droppedRecords = append(droppedRecords, dropped...)
+		errors = append(errors, err)
+	}
+
+	if len(droppedRecords) > 0 {
+		// Move all dropped records to Logs
+		droppedLogs := pdata.NewLogs()
+		droppedLogs.ResourceLogs().Resize(1)
+		droppedLogs.ResourceLogs().At(0).InstrumentationLibraryLogs().Resize(1)
+		for _, record := range droppedRecords {
+			droppedLogs.ResourceLogs().At(0).InstrumentationLibraryLogs().At(0).Logs().Append(record)
+		}
+
+		// ToDo: uncomment when https://github.com/open-telemetry/opentelemetry-collector/pull/2059 is used globally
+		// return len(droppedRecords), consumererror.PartialLogsError(componenterror.CombineErrors(errors), droppedLogs)
+		return len(droppedRecords), componenterror.CombineErrors(errors)
+	}
+
 	return 0, nil
 }
